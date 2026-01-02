1e777b818370a47b80dfc1032225b49e18d329d4
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-03 00:55:59 +0900

    init: 절대 경로 설정 (#7)

---
+++ src/pages/home/ui/home-page.tsx
@@ -0,0 +1,12 @@
+import KERORO from "@images/comfit_web_상태.jpg";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <img src={KERORO} alt="Keroro" />
+    </div>
+  );
+};
+
+export { HomePage };
