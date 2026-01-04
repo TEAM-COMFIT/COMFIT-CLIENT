a3d229af8c2f4e2622f8b7d0a63b3f15955d1f07
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-04 17:31:09 +0900

    chore: unused 코드 삭제 (#10)

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import importPlugin from "eslint-plugin-import";
 import typescriptParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
