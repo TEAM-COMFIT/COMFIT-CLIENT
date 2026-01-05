b9c6bfdb77ce6ea51cc1927eea684a7753034ffa
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-06 03:32:29 +0900

    feat: vanilla extract 설치 (#13)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import { HomePage } from "@/pages/home/home-page";
+import { appContainer } from "./App.css";
 
 const App = () => {
   return (
-    <>
+    <div className={appContainer}>
       <HomePage />
-    </>
+    </div>
   );
 };
 
