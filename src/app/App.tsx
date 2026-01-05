fa9516c004dd4d71907ae768e758a22021f685fe
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-06 04:22:23 +0900

    feat: 토큰 추가 (#13)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import { HomePage } from "@/pages/home/home-page";
-import { appContainer } from "./App.css";
 
 const App = () => {
   return (
-    <div className={appContainer}>
+    <>
       <HomePage />
-    </div>
+    </>
   );
 };
 
