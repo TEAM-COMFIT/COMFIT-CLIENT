b0e6a1c02790d17c326e2bad1cb7e102ce51d31b
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-06 01:09:01 +0900

    feat: appRoutes 추가 (#12)

--- src/app/routes/appRoutes.tsx
+++ src/app/routes/appRoutes.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import { publicRoutes } from "./public-routes";
 import { protectedRoutes } from "./protected-routes";
 
-export const router = createBrowserRouter({
-  ...publicRoutes,
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <RootLayout />, TODO: RootLayout 추가
+    children: [
+      ...publicRoutes,
 
-  // TODO: auth에 따른 처리 추가
-  ...protectedRoutes,
-
-  // TODO: paths 이외의 경로 접근 시 error 처리
-});
+      // TODO: auth에 따른 처리 추가
+      ...protectedRoutes,
+      // TODO: paths 이외의 경로 접근 시 error 처리
+    ],
+  },
+]);
