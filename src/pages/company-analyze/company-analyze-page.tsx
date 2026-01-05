d9446f1a91f261bc23af8d6bd631171cc8464905
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-05 19:51:37 +0900

    add: 주요 페이지 파일 추가 (#12)

---
+++ src/pages/company-analyze/company-analyze-page.tsx
@@ -0,0 +1,9 @@
+const CompanyAnalyzePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Company Analyze Page</h1>
+    </div>
+  );
+};
+
+export { CompanyAnalyzePage };
---
+++ src/pages/experience/experience-page.tsx
@@ -0,0 +1,9 @@
+const ExperiencePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Experience Page</h1>
+        </div>
+    );
+};
+
+export { ExperiencePage };
