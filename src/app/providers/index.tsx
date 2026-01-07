a5aa99627e7a55d95352c62571049d9f9bf6910e
hello-yujin <yuuuujiiiin@naver.com>
2026-01-07 22:56:17 +0900

    refactor: 전역 Provider 엔트리 분리 및 main.tsx 구조 정리

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from "react";
+
+import QueryProvider from "./QueryProvider";
+
+export default function AppProviders({ children }: PropsWithChildren) {
+  return <QueryProvider>{children}</QueryProvider>;
+}
