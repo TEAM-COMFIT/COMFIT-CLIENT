3cfcd6da30d5e5f4060f037d9f2deb0412a8cc7c
qowjdals23 <jmb0331@naver.com>
2026-01-08 18:27:21 +0900

    fix: 스토리북 파일 삭제 복구 (#16)

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import { mergeConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
+  addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
+
+  framework: "@storybook/react-vite",
+
+  viteFinal: async (storybookConfig) =>
+    mergeConfig(storybookConfig, {
+      plugins: [tsconfigPaths()],
+    }),
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldRemoveUndefinedFromOptional: true,
+      propFilter: (prop) => !/node_modules/.test(prop.parent?.fileName || ""),
+    },
+  },
+};
+
+export default config;
