35d258ff7cea001373f73186e5026448c4fcb4d0
hello-yujin <yuuuujiiiin@naver.com>
2026-01-09 02:50:07 +0900

    refactor: App.tsx auth store 초기화와 중복된 syncFromStorage 호출 제거

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from 'react';
-
 import { HomePage } from '@pages/home/home-page';
-import { useAuthStore } from '@shared/model/auth';
 
 const App = () => {
-  const syncFromStorage = useAuthStore((s) => s.actions.syncFromStorage);
-
-  useEffect(() => {
-    syncFromStorage();
-  }, [syncFromStorage]);
-
   return (
     <>
       <HomePage />
