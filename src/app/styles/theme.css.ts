f3955a5db9ce98b2f598f4b9b6f007e38d8a758c
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-09 02:24:06 +0900

    feat: sprinkles 라이브러리 제거 (#13)

--- src/app/styles/theme.css.ts
+++ src/app/styles/theme.css.ts
@@ -1,19 +1,15 @@
 import { createTheme } from "@vanilla-extract/css";
-import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
 
 import { fontStyles } from "@/shared/styles/font-style.css";
 import { color } from "@/shared/styles/tokens/color.css";
 
+// 테마 토큰 정의
 const tokens = {
   color: color,
   fontStyles: fontStyles,
 };
 
-const properties = defineProperties({
-  properties: tokens,
-});
-const sprinkles = createSprinkles(properties);
-
+// color와 fontStyle 테마로 생성
 const [themeClass, themeVars] = createTheme(tokens);
 
-export { sprinkles, themeClass, themeVars, tokens };
+export { themeClass, themeVars, tokens };
