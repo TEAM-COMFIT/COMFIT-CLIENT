b68840fa7ce3e491c597a426b787fcbeb528649d
odukong <ohbin1017@gmail.com>
2026-01-10 00:54:44 +0900

    update: query-client option 및 공통 에러 핸들러 수정 (#17)
    - query-client 전역 설정 추가 (throwOnError 옵션 조정)
    - query(GET)와 mutation(POST 등)의 에러 처리 로직 분리
      - query: ErrorBoundary로 에러 전파 (Suspense 위함)
      - mutation: 토스트 메시지 등 UI 피드백을 위해 에러 전파 방지
    - 단, useSuspeseQuery는 handleApiError를 거치지 않고 바로 errorboundary가 잡게 됨.

--- src/shared/api/error-handler.ts
+++ src/shared/api/error-handler.ts
@@ -9,8 +9,11 @@ import type {
 } from "@/shared/api/types/error-response";
 import type { AxiosError } from "axios";
 
-// 서버 정의 에러 타입 가드
-// 서버가 보낸 에러가 맞는지 확인하는 함수
+/**
+ * 서버 정의 에러(Business Error) 타입 가드
+ * - HTTP 상태 코드는 정상이지만, 비즈니스 로직상 실패한 경우를 식별합니다.
+ * - 응답 본문(data)에 'prefix' 필드가 있는지 확인합니다. (추후 TODO)
+ */
 export const isValidCustomError = (
   error: unknown
 ): error is RequiredWith<AxiosError<ApiErrorResponse>, "response"> => {
@@ -22,15 +25,26 @@ export const isValidCustomError = (
   );
 };
 
-// 공통 에러 핸들러
-export const handleApiError = (error: unknown) => {
+// 요청 종류 구분 (UI 피드백 방식을 결정하기 위함)
+type ErrorType = "query" | "mutation";
+
+/**
+ * API 공통 에러 핸들러
+ * @param error 발생한 에러 객체
+ * @param type 요청 종류 ('query': 조회 -> 에러페이지 / 'mutation': 수정 -> 토스트)
+ */
+export const handleApiError = (error: unknown, type: ErrorType) => {
   if (isValidCustomError(error)) {
     const { prefix } = error.response.data;
     const message =
       ERROR_MESSAGE[prefix as ApiErrorCode] ??
       "알 수 없는 에러가 발생했습니다.";
+
     console.error(message);
-    return message;
+    if (type == "mutation") {
+      // TODO: toast 메시지 handleApiError 내부처리 or message return 후 컴포넌트에서 처리
+      return message;
+    }
   } else if (isAxiosError(error)) {
     console.error("서버/네트워크 오류입니다");
     // TODO: 에러 페이지 필요
