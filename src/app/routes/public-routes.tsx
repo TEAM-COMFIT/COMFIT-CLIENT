196cd86924b2605af1831d4440b8b905f23e961b
odukong <ohbin1017@gmail.com>
2026-01-13 04:59:38 +0900

    fix: public, private 경로 수정 (#38)
    - 온보딩(소개), 홈, 상세페이지 경로를 public 경로로 수정하였습니다.

--- src/app/routes/public-routes.tsx
+++ src/app/routes/public-routes.tsx
@@ -1,5 +1,14 @@
+import { CompanyDetailPage } from "@/pages/company-detail/company-detail-page";
+import { HomePage } from "@/pages/home/home-page";
 import { LoginPage } from "@/pages/login/login-page";
+import { OnboardingPage } from "@/pages/onboarding/onboarding-page";
 
 import { ROUTES } from "./paths";
 
-export const publicRoutes = [{ path: ROUTES.LOGIN, element: <LoginPage /> }];
+export const publicRoutes = [
+  { path: ROUTES.LOGIN, element: <LoginPage /> },
+  { path: ROUTES.ONBOARDING, element: <OnboardingPage /> },
+
+  { path: ROUTES.HOME, element: <HomePage /> },
+  { path: ROUTES.COMPANY(), element: <CompanyDetailPage /> },
+];
