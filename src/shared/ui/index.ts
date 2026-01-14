f57c69ddf7e6a360cff433f038021cb212cacfbf
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-14 21:47:43 +0900

    feat: select 공통 컴포넌트 구현 (#51)

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,1 +1,14 @@
-export * from "./dropdown";
+export { Dropdown } from "./dropdown/dropdown";
+
+export { Pagination } from "./pagination/pagination";
+export { usePagination } from "./pagination/use-pagination";
+
+export { Modal } from "./modal/modal";
+export { ModalBasic } from "./modal/modal-basic";
+export { useModal } from "./modal/use-modal";
+
+export { Button } from "./button/button";
+
+export { Select } from "./select/select";
+
+export { Tag } from "./tag/tag";
---
+++ src/shared/ui/select/select.css.ts
@@ -0,0 +1,82 @@
+import { style, styleVariants } from "@vanilla-extract/css";
+
+import { themeVars } from "@/app/styles";
+
+/* ---------- Wrapper ---------- */
+export const selectWrapper = style({
+  position: "relative",
+  display: "inline-flex",
+  flexDirection: "column",
+});
+
+/* ---------- Trigger ---------- */
+export const trigger = style({
+  display: "flex",
+  alignItems: "center",
+  width: "88rem",
+  height: "6rem",
+  justifyContent: "space-between",
+  padding: "0 2rem 0 1.6rem",
+  color: themeVars.color.gray400,
+  backgroundColor: themeVars.color.white,
+  border: `1.5px solid ${themeVars.color.normal}`,
+  borderRadius: "12px",
+  selectors: {
+    "&:focus": {
+      color: themeVars.color.gray800,
+    },
+  },
+
+  ...themeVars.fontStyles.hline_m_18,
+});
+
+/* ---------- Arrow Icon ---------- */
+export const arrowIcon = style({
+  width: "1.8rem",
+  height: "1.8rem",
+  color: themeVars.color.gray800,
+});
+
+export const arrowIconTransition = styleVariants({
+  closed: {
+    transform: "rotate(180deg)",
+  },
+  open: {
+    transform: "rotate(0deg)",
+  },
+});
+
+/* ---------- Menu ---------- */
+export const menu = style({
+  position: "absolute",
+  top: "100%",
+  width: "100%",
+  marginTop: "0.8rem",
+  padding: "0.8rem",
+  backgroundColor: themeVars.color.white,
+  border: `1.5px solid ${themeVars.color.normal}`,
+  borderRadius: "8px",
+  zIndex: themeVars.zIndex.dropdownMenu,
+});
+
+/* ---------- Item ---------- */
+export const item = style({
+  padding: "1rem 1.2rem",
+  width: "100%",
+  borderRadius: "8px",
+  textAlign: "left",
+  cursor: "pointer",
+  margin: "0.4rem",
+  backgroundColor: themeVars.color.white,
+  ...themeVars.fontStyles.body_r_16,
+
+  selectors: {
+    "&:hover": {
+      backgroundColor: themeVars.color.blue200,
+    },
+    "&:pressed": {
+      backgroundColor: themeVars.color.blue600,
+      color: themeVars.color.white,
+    },
+  },
+});
