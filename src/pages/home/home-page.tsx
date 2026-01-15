83ffbdeb8f23aa1e4d9155e268db42c84de2f552
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-15 13:54:22 +0900

    chore: 안 쓰는 import 삭제 (#47)

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,12 +1,9 @@
-import { CompanyGridContainer } from "@/shared/ui/pagination/company-list-container";
-import { MatchingListContainer } from "@/shared/ui/pagination/matching-list-container";
 import { RefreshButton } from "@/widgets/refresh-button/refresh-button";
 import Heart from "@icons/heart.svg?react";
 import KERORO from "@images/comfit_web_status.jpg";
 
 import { appContainer } from "./home-page.css";
 const HomePage = () => {
-
   return (
     <div className={appContainer}>
       <h1>프리텐다드</h1>
