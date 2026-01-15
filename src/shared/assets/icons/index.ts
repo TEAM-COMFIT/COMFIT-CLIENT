d2716ea801b5f9495cebd9f2433069f4562e9140
odukong <ohbin1017@gmail.com>
2026-01-16 04:06:35 +0900

    add: 로그인 페이지 퍼블리싱 요소 추가 (#76)

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,4 +1,3 @@
-
 export { default as DropdownArrow } from "./dropdown_up_arrow.svg?react";
 export { default as Avatar } from "./avatar.svg?react";
 
@@ -6,3 +5,5 @@ export { default as IconArrowLeft } from "./arrow_left.svg?react";
 export { default as IconArrowRight } from "./arrow_right.svg?react";
 export { default as IconDoubleArrowLeft } from "./double_arrow_left.svg?react";
 export { default as IconDoubleArrowRight } from "./double_arrow_right.svg?react";
+
+export { default as IconKakao } from "./icon_kakao.svg?react";
--- src/shared/styles/tokens/color.css.ts
+++ src/shared/styles/tokens/color.css.ts
@@ -32,6 +32,10 @@ export const color = {
   purple300: "#E196F7",
   purple400: "#BE4FE3",
 
+  // Kakao color
+  kakaoclr: "#191919",
+  kakaobg: "#FEE500",
+
   // White / Black
   white: "#FFFFFF",
   black: "#000000",
