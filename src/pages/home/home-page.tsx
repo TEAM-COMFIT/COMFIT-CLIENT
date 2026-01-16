974d27da79c44dfcee66bd792e2793635ddf51a6
hello-yujin <yuuuujiiiin@naver.com>
2026-01-17 01:30:24 +0900

    chore: 주석 정리 (#75)

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
       <h1>프리텐다드</h1>
       <p>카카오로 시작하기</p>
       <img src={KERORO} alt="Keroro" width={400} />
-      // 관심 분야 선택 컴포넌트 확인용
+      {/* 관심 분야 선택 컴포넌트 확인용 */}
       <section style={{ marginTop: 40, display: "grid", gap: 40 }}>
         <IndustryInterestSelect />
         <JobInterestSelect />
