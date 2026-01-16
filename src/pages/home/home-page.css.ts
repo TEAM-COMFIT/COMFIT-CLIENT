dc7196d1e7a2166bd40cc0025aab20cd2b8a6b97
qowjdals23 <jmb0331@naver.com>
2026-01-17 02:31:23 +0900

    chore: 예시 스타일 코드 삭제 (#33)

--- src/pages/home/home-page.css.ts
+++ src/pages/home/home-page.css.ts
@@ -1,4 +1,4 @@
-import { style, styleVariants } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 
 import { themeVars } from "@/app/styles";
 
@@ -9,68 +9,4 @@ export const appContainer = style({
   justifyContent: "center",
   color: themeVars.color.blue300,
   ...themeVars.fontStyles.title_b_28,
-});
-
-export const textfieldPreviewSection = style({
-  marginTop: "6rem",
-  display: "flex",
-  flexDirection: "column",
-  gap: "2.4rem",
-});
-
-export const textfieldPreviewHeader = style({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  gap: "1.6rem",
-});
-
-export const textfieldPreviewTitle = style({
-  ...themeVars.fontStyles.hline_m_18,
-  color: themeVars.color.gray800,
-});
-
-export const textfieldPreviewToggle = style({
-  display: "inline-flex",
-  gap: "0.8rem",
-});
-
-export const textfieldPreviewToggleButton = style({
-  height: "4rem",
-  padding: "0.8rem 1.2rem",
-
-  borderRadius: "12px",
-  border: `1.5px solid ${themeVars.color.normal}`,
-  backgroundColor: themeVars.color.white,
-
-  cursor: "pointer",
-
-  ...themeVars.fontStyles.body_r_16,
-  color: themeVars.color.gray800,
-});
-
-export const textfieldPreviewToggleButtonState = styleVariants({
-  inactive: {},
-  active: {
-    borderColor: themeVars.color.blue600,
-    backgroundColor: themeVars.color.blue200,
-    color: themeVars.color.blue600,
-  },
-});
-
-export const textfieldPreviewList = style({
-  display: "flex",
-  flexDirection: "column",
-  gap: "3.2rem",
-});
-
-export const textfieldPreviewItem = style({
-  display: "flex",
-  flexDirection: "column",
-  gap: "0.8rem",
-});
-
-export const textfieldPreviewLabel = style({
-  ...themeVars.fontStyles.body_m_16,
-  color: themeVars.color.gray800,
 });
