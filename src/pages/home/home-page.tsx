fc2893e7b8bcf292fcaf0646bd4a3d8726454686
qowjdals23 <jmb0331@naver.com>
2026-01-17 04:11:51 +0900

    chore: 예시 코드 추가 (#33)

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
+import { Textfield } from "@/shared/ui/textfield";
 import KERORO from "@images/comfit_web_status.jpg";
 
 import { appContainer } from "./home-page.css";
 
 const HomePage = () => {
+  const [description, setDescription] = useState("");
+  const isEdit = true;
+
   return (
     <div className={appContainer}>
       <h1>프리텐다드</h1>
       <p>카카오로 시작하기</p>
       <img src={KERORO} alt="Keroro" width={400} />
+      <Textfield
+        type="jobDescription"
+        placeholder="자기소개를 입력해주세요."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        mode={isEdit ? "edit" : "view"}
+      />
       <p>하하코드래빗아 한번일해보거라</p>
     </div>
   );
