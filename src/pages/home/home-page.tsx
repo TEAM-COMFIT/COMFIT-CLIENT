8e62c379fc013653bc119f8cf088810aa8e2b2ba
qowjdals23 <jmb0331@naver.com>
2026-01-16 17:37:58 +0900

    fix: home-page 예시 코드 삭제  (#33)

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,66 +1,7 @@
-import { useState } from "react";
-
-import { CompanyGridContainer } from "@/shared/ui/pagination/company-list-container";
-import { MatchingListContainer } from "@/shared/ui/pagination/matching-list-container";
-import { Textfield } from "@/shared/ui/textfield";
 import Heart from "@icons/heart.svg?react";
 import KERORO from "@images/comfit_web_status.jpg";
 
-import {
-  appContainer,
-  textfieldPreviewHeader,
-  textfieldPreviewItem,
-  textfieldPreviewLabel,
-  textfieldPreviewList,
-  textfieldPreviewSection,
-  textfieldPreviewTitle,
-  textfieldPreviewToggle,
-  textfieldPreviewToggleButton,
-  textfieldPreviewToggleButtonState,
-} from "./home-page.css";
-
-import type { ChangeEventHandler } from "react";
-
-type Mode = "edit" | "view";
-
-type TextfieldKey =
-  | "jobDescription"
-  | "situation"
-  | "task"
-  | "result"
-  | "action";
-
-const PLACEHOLDER =
-  "ex) 대학생 마케팅 동아리에서 신규 브랜드 인지도를 높이기 위한 프로젝트를 진행함.";
-
-type TextfieldFieldProps = {
-  type: TextfieldKey;
-  label: string;
-  placeholder?: string;
-  mode: Mode;
-  value: string;
-  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
-};
-
-const TextfieldField = ({
-  label,
-  type,
-  placeholder,
-  mode,
-  value,
-  onChange,
-}: TextfieldFieldProps) => (
-  <div className={textfieldPreviewItem}>
-    <p className={textfieldPreviewLabel}>{label}</p>
-    <Textfield
-      type={type}
-      mode={mode}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
-  </div>
-);
+import { appContainer } from "./home-page.css";
 
 const HomePage = () => {
   return (
