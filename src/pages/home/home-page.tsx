059082f99a4d6c36150681d33507e6995dc4ede7
hello-yujin <yuuuujiiiin@naver.com>
2026-01-17 01:28:34 +0900

    feat: 관심 분야 선택 컴포넌트 테스트를 위한 home 페이지 연결 (#75)

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,14 +1,22 @@
+import {
+  IndustryInterestSelect,
+  JobInterestSelect,
+} from "@/features/onboarding";
 import KERORO from "@images/comfit_web_status.jpg";
 
 import { appContainer } from "./home-page.css";
 
 const HomePage = () => {
-
   return (
     <div className={appContainer}>
       <h1>프리텐다드</h1>
       <p>카카오로 시작하기</p>
       <img src={KERORO} alt="Keroro" width={400} />
+      // 관심 분야 선택 컴포넌트 확인용
+      <section style={{ marginTop: 40, display: "grid", gap: 40 }}>
+        <IndustryInterestSelect />
+        <JobInterestSelect />
+      </section>
     </div>
   );
 };
