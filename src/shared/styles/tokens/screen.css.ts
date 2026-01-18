9cbef7642378dc6312840a5d2675668eb11d129f
odukong <ohbin1017@gmail.com>
2026-01-18 17:35:35 +0900

    feat: 반응형, 그라데이션 토큰 추가 (#89)

---
+++ src/shared/styles/tokens/screen.css.ts
@@ -0,0 +1,13 @@
+import type { CSSProperties } from "react";
+
+export const screenBreakpoints = {
+  mobile: "(min-width: 375px)",
+} as const;
+
+export const screen = {
+  mobile: (css: CSSProperties) => ({
+    "@media": {
+      "(min-width: 375px)": css,
+    },
+  }),
+} as const;
