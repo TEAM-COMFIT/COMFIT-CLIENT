8fb10dd776d5b9104b058aaea0e786ad4a880109
odukong <ohbin1017@gmail.com>
2026-01-20 05:28:56 +0900

    fix: 아이콘 문제 해결 (#89)

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -7,6 +7,7 @@ export { default as InfoIcon } from "./i.svg?react";
 export { default as IconCompany } from "./icon_company.svg?react";
 
 export { default as RefreshIcon } from "./refresh_icon.svg?react";
+
 export { default as DropdownArrow } from "./dropdown_up_arrow.svg?react";
 export { default as Avatar } from "./avatar.svg?react";
 
@@ -14,3 +15,28 @@ export { default as IconArrowLeft } from "./arrow_left.svg?react";
 export { default as IconArrowRight } from "./arrow_right.svg?react";
 export { default as IconDoubleArrowLeft } from "./double_arrow_left.svg?react";
 export { default as IconDoubleArrowRight } from "./double_arrow_right.svg?react";
+
+export { default as IconCancel } from "./cancel.svg?react";
+export { default as IconMove } from "./move_detail_page_icon.svg?react";
+
+export { default as IconLinkDefault } from "./icon_link_default.svg?react";
+export { default as IconInformation } from "./icon_information.svg?react";
+export { default as IconQuestion } from "./icon_question.svg?react";
+export { default as IconCalendar } from "./icon_date.svg?react";
+
+export { default as DateLeftArrow } from "./date_arrow_left.svg?react";
+export { default as DateRightArrow } from "./date_arrow_right.svg?react";
+export { default as IconDash } from "./line.svg?react";
+export { default as IconCheck } from "./check.svg?react";
+
+export { default as IconAI } from "./icon_ai.svg?react";
+
+export { default as IconMatchBefore } from "./icon_match_before.svg?react";
+export { default as IconAIBefore } from "./icon_ai_before.svg?react";
+export { default as IconEXPBefore } from "./icon_exp_before.svg?react";
+
+export { default as IconX } from "./x.svg?react";
+
+export { default as LOGO } from "./comfit_logo.svg?react";
+
+export { default as IconKakao } from "./icon_kakao.svg?react";
