f73822bd947d39e650c56e8caa768b9f8afef723
hello-yujin <yuuuujiiiin@naver.com>
2026-01-21 02:43:15 +0900

    chore: experience-detail public exports 정리 (#108)

--- src/features/experience-detail/index.ts
+++ src/features/experience-detail/index.ts
@@ -1,3 +1,79 @@
 export { DatePicker } from "./ui/date-picker/date-picker";
 export { ExperienceForm } from "./ui/experience-form/experience-form";
 export { ExperienceViewer } from "./ui/experience-viewer/experience-viewer";
+export { ExperienceAlertRenderer } from "./ui/experience-alert-renderer";
+
+export { useExperienceDetailStore, initialDraft } from "./store/store";
+
+export {
+  useExperienceMode,
+  useExperienceCurrent,
+  useExperienceDraft,
+  useDefaultExperienceId,
+  useExperienceActions,
+  useIsDraftDefault,
+  useDefaultButtonLabel,
+  useShowEditDeleteButtons,
+  useShowSubmitButton,
+  useCurrentExperienceId,
+  useDraftTitle,
+  useDraftType,
+  useDraftStartAt,
+  useDraftEndAt,
+  useDraftSituation,
+  useDraftTask,
+  useDraftAction,
+  useDraftResult,
+} from "./store/selectors";
+
+export {
+  useExperienceSubmit,
+  useExperienceHeaderActions,
+  useDeleteExperience,
+} from "./model/use-actions";
+
+export {
+  formatToYMD,
+  parseYMD,
+  useExperienceDateField,
+} from "./lib/date-field";
+
+export {
+  showExperienceError,
+  showExperienceSuccess,
+  showExperienceInfo,
+  showExperienceWarning,
+  showValidationError,
+  showSaveError,
+  showDeleteError,
+  showDefaultSettingError,
+  showSaveSuccess,
+  showDeleteSuccess,
+  useExperienceAlerts,
+  useExperienceAlertActions,
+} from "./model/use-alert";
+
+export {
+  EXPERIENCE_MOCK,
+  initExperienceDetail,
+  resetExperienceDetail,
+} from "./model/use-init";
+
+export { validateExperienceDraft } from "./lib/validation";
+
+export type {
+  ExperienceMode,
+  ExperienceType,
+  ExperienceUpsertBody,
+  ExperienceEntity,
+  DefaultExperience,
+} from "./types";
+
+export {
+  EXPERIENCE_ROUTES,
+  EXPERIENCE_MESSAGES,
+  DEFAULT_BUTTON_LABELS,
+  EXPERIENCE_TYPE_OPTIONS,
+  EXPERIENCE_LABEL_TO_CODE,
+  EXPERIENCE_TYPE_LIST,
+} from "./config";
