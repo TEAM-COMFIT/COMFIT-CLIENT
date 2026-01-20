42b32904ad2837b51e5d6462dc67f382d888b7bd
hello-yujin <yuuuujiiiin@naver.com>
2026-01-20 19:00:37 +0900

    chore: experience-detail feature public API(index.ts) 추가 (#108)

--- src/features/experience-detail/index.ts
+++ src/features/experience-detail/index.ts
@@ -1,1 +1,3 @@
 export { DatePicker } from "./ui/date-picker/date-picker";
+export { ExperienceForm } from "./ui/experience-form/experience-form";
+export { ExperienceViewer } from "./ui/experience-viewer/experience-viewer";
