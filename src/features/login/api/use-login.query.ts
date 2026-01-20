93c5bae38ce7175c6a0997f5a34925cafc2c4e27
odukong <ohbin1017@gmail.com>
2026-01-20 18:21:03 +0900

    fix: 예시 코드 삭제 (#106)

--- src/features/login/api/use-login.query.ts
+++ src/features/login/api/use-login.query.ts
@@ -2,8 +2,6 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { api } from "@/shared/api/axios-instance";
 
-import type { GetCompanyResponseDto } from "@/shared/api/generate/http-client";
-
 // API 요청 함수
 export const getLogin = async (code: string) => {
   const response = await api.oauth.kakaoCallback({ code }, { secure: false });
@@ -14,20 +12,5 @@ export const useLogin = (code: string) => {
   return useSuspenseQuery({
     queryKey: ["kakao-login", code],
     queryFn: () => getLogin(code),
-  });
-};
-
-// /// 예시 코드
-export const getInfo = async (id: number) => {
-  const response = await api.companies.getSuggestionCompany(id, {
-    secure: false,
-  });
-  return response.result as unknown as GetCompanyResponseDto[];
-};
-
-export const useGetCompany = (id: number) => {
-  return useSuspenseQuery({
-    queryKey: ["suggestion-company", id],
-    queryFn: () => getInfo(id),
   });
 };
