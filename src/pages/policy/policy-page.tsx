aa3002f87f6e39e4e86eaf37754bb8937f58ffaf
odukong <ohbin1017@gmail.com>
2026-03-22 16:32:58 +0900

    refactor: policypage 중복 코드 제거 (#159)

--- src/pages/policy/policy-page.tsx
+++ src/pages/policy/policy-page.tsx
@@ -6,34 +6,41 @@ interface PolicyPageProps {
   mode: "USE" | "PRIVACY";
 }
 
+const POLICY_CONTENT = {
+  USE: {
+    title: "이용약관",
+    subTitle:
+      "컴핏 관련 제반 서비스의 이용과 관련하여 필요한 사항을 규정합니다.",
+    gap: 24 as const,
+    Component: <UsePolicy />,
+  },
+  PRIVACY: {
+    title: "개인정보처리방침",
+    subTitle: "컴핏은 개인정보 보호 등에 관한 법률을 준수합니다.",
+    gap: 40 as const,
+    Component: <PrivacyPolicy />,
+  },
+};
+
 const PolicyPage = ({ mode }: PolicyPageProps) => {
+  const currentPolicy = POLICY_CONTENT[mode];
+
+  if (!currentPolicy) return <></>;
+
   return (
     <div className={styles.background}>
-      {mode === "USE" ? (
-        <div className={`${styles.wrapper} ${styles.flexColumn({ gap: 24 })}`}>
-          <div className={styles.flexColumn({ gap: 16 })}>
-            <h1 className={styles.title}>이용약관</h1>
-            <p className={styles.subTitle}>
-              컴핏 관련 제반 서비스의 이용과 관련하여 필요한 사항을 규정합니다.
-            </p>
-          </div>
-          <div className={styles.divider} />
-          <UsePolicy />
+      <div
+        className={`${styles.wrapper} ${styles.flexColumn({ gap: currentPolicy.gap })}`}
+      >
+        {/** 약관 타이틀 */}
+        <div className={styles.flexColumn({ gap: 16 })}>
+          <h1 className={styles.title}>{currentPolicy.title}</h1>
+          <p className={styles.subTitle}>{currentPolicy.subTitle}</p>
         </div>
-      ) : mode === "PRIVACY" ? (
-        <div className={`${styles.wrapper} ${styles.flexColumn({ gap: 40 })}`}>
-          <div className={styles.flexColumn({ gap: 16 })}>
-            <h1 className={styles.title}>개인정보처리방침</h1>
-            <p className={styles.subTitle}>
-              컴핏은 개인정보 보호 등에 관한 법률을 준수합니다.
-            </p>
-          </div>
-          <div className={styles.divider} />
-          <PrivacyPolicy />
-        </div>
-      ) : (
-        <></>
-      )}
+        <div className={styles.divider} />
+        {/** 약관 컨텐츠 */}
+        {currentPolicy.Component}
+      </div>
     </div>
   );
 };
